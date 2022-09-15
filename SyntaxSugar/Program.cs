d0c850603cf2dc043a0a8fd8afb1f3ce99506bc2
samanthakyra <samanthakyramendoza@gmail.com>
2022-09-15 08:56:34 -0700

    initial commit

--- SyntaxSugar/Program.cs
+++ SyntaxSugar/Program.cs
@@ -6,7 +6,20 @@ namespace SyntaxSugar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int answer = 4;
+            //string response;
+            //if( (answer < 9)
+            //    {
+            //    response = answer + " is less than 9";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to 9";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? "{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
         }
     }
 }
